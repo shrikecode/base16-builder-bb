4ebdeaf66735a3616779d7e68ec948970d559c08
Wojciech M. Wnuk <laniusone@pm.me>
2024-06-29 16:05:00 +0200

    do not print from render, slurp scheme if file provided as an arg

--- src/base16/core.clj
+++ src/base16/core.clj
@@ -10,18 +10,18 @@
 (defn render [template scheme]
   (let [scheme (yaml/parse-string scheme)]
     ;; why vars in templates are different than in schemes??!!
-    (println (pg/render-string
+    (pg/render-string
      (slurp template)
      (set/rename-keys
       scheme
       (zipmap
        (keys scheme)
-       (map (fn [key] (str/join "" (list (name key) "-hex"))) (keys scheme))))))))
+       (map (fn [key] (str/join "" (list (name key) "-hex"))) (keys scheme)))))))
 
 (defn -main [& args]
   (let [{:keys [template scheme]} (cli/parse-opts args)]
     (if scheme
-      (println (render template scheme))
+      (println (render template (slurp scheme)))
       (with-open [r (io/reader *in*)]
-        (render template (str/join "\n" (line-seq r)))
+        (println (render template (str/join "\n" (line-seq r))))
         ))))
